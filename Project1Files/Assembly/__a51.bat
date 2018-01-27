1643729bc93193bbf6db0c726609a1d1dd1abc11
Huntington Shin Jasperson <huntingtonjasperson@gmail.com>
2018-01-27 15:51:17 -0800

    added reflow_over_controller.asm which is a barebones assembly file. just prints hello world and has interrupts included but not implemented.

---
+++ Project1Files/Assembly/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+D:
+cd "\Users\hunti_000\Documents\repos\elec291group\Project1Files\Assembly\"
+"D:\Programs\CrossIDE\Call51\Bin\a51.exe" -l "D:\Users\hunti_000\Documents\repos\elec291group\Project1Files\Assembly\reflow_oven_controller.asm"
+echo Crosside_Action Set_Hex_File D:\Users\hunti_000\Documents\repos\elec291group\Project1Files\Assembly\reflow_oven_controller.HEX
