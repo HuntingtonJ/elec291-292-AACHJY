6be674cb8eb55ed485efe83bfa050b0240433c93
Huntington Shin Jasperson <huntingtonjasperson@gmail.com>
2018-02-06 13:42:01 -0800

    Finished Timer1 init. Cleared out excess routine code. Testing for pwm

--- Project1Files/Assembly/PWM_code.asm
+++ Project1Files/Assembly/PWM_code.asm
@@ -28,7 +28,7 @@ org 0x0003
 
 ; Timer/Counter 0 overflow interrupt vector
 org 0x000B
-	ljmp Timer0_ISR
+	reti
 
 ; External interrupt 1 vector (not used in this code)
 org 0x0013
@@ -44,7 +44,7 @@ org 0x0023
 	
 ; Timer/Counter 2 overflow interrupt vector
 org 0x002B
-	ljmp Timer2_ISR
+	reti
 	
 DSEG at 0x30
 Count1ms:      ds 2 ; Used to determine when half second has passed
@@ -58,39 +58,6 @@ CSEG
 	
 ;---------------------------------;
 ; Routine to initialize the ISR   ;
-; for timer 0                     ;
-;---------------------------------;
-Timer0_Init:
-	mov a, TMOD
-	anl a, #0xf0 ; Clear the bits for timer 0
-	orl a, #0x01 ; Configure timer 0 as 16-timer
-	mov TMOD, a
-	mov TH0, #high(TIMER0_RELOAD)
-	mov TL0, #low(TIMER0_RELOAD)
-	; Set autoreload value
-	mov TIMER0_RELOAD_H, #high(TIMER0_RELOAD)
-	mov TIMER0_RELOAD_L, #low(TIMER0_RELOAD)
-	; Enable the timer and interrupts
-    setb ET0  ; Enable timer 0 interrupt   
-    setb TR0  ; Start timer 0
-	ret
-
-;---------------------------------;
-; ISR for timer 0.  Set to execute;
-; every 1/4096Hz to generate a    ;
-; 2048 Hz square wave at pin P3.7 ;
-;---------------------------------;
-Timer0_ISR:
-	;clr TF0  ; According to the data sheet this is done for us already.
-	sjmp no_beep
-beep_on:
-	;cpl SOUND_OUT ; Connect speaker to P3.7!
-no_beep:
-	reti
-	
-	
-;---------------------------------;
-; Routine to initialize the ISR   ;
 ; for timer 1                     ; 
 ;---------------------------------;
 Timer1_Init:
@@ -99,59 +66,23 @@ Timer1_Init:
 	orl a, #00010000B
 	mov TMOD, a
 	
-	mov TH1, DUTY_CYCLE
-	mov TL1, 
+	mov TH1, DUTY_50             ;Current count value
+	mov TL1,                     ;Linear Prescaling
+	
+	mov TIMER1_RELOAD_H, DUTY_50 ;Duty cycle percentage
+	mov TIMER1_RELOAD_L, 255     ;Frequency scaling/adjust f_out = f_sys/(256 * (256 - TL))
 	
 	mov a, TCONB ;load TCONB for PWM settings
-	anl 
+	anl a, #00000000B
+	orl a, #10000000B
+	mov TCONB, a
 	
-	
-
-
-;---------------------------------;
-; Routine to initialize the ISR   ;
-; for timer 2                     ;
-;---------------------------------;
-Timer2_Init:
-	mov T2CON, #0 ; Stop timer/counter.  Autoreload mode.
-	mov TH2, #high(TIMER2_RELOAD)
-	mov TL2, #low(TIMER2_RELOAD)
-	; Set the reload value
-	mov RCAP2H, #high(TIMER2_RELOAD)
-	mov RCAP2L, #low(TIMER2_RELOAD)
-	; Init One millisecond interrupt counter.  It is a 16-bit variable made with two 8-bit parts
-	clr a
-	mov Count1ms+0, a
-	mov Count1ms+1, a
-	; Enable the timer and interrupts
-    setb ET2  ; Enable timer 2 interrupt
-    setb TR2  ; Enable timer 2
-	ret
-
-;---------------------------------;
-; ISR for timer 2                 ;
-;---------------------------------;
-Timer2_ISR:
-	clr TF2  ; Timer 2 doesn't clear TF2 automatically. Do it in ISR
-	cpl P3.6 ; To check the interrupt rate with oscilloscope. It must be precisely a 1 ms pulse.
-	
-	; The two registers used in the ISR must be saved in the stack
-	push acc
-	push psw
-	
-	; Increment the 16-bit one mili second counter
-	inc Count1ms+0    ; Increment the low 8-bits first
-	mov a, Count1ms+0
-	cjne a, #10h, Timer2_ISR_done
-	mov Count1ms+0, #0h
-	
-Timer2_ISR_done:
-	pop psw
-	pop acc
+	setb TR1
 	reti
 	
 MainProgram:
     mov SP, #7FH ; Set the stack pointer to the begining of idata
+    lcall Timer1_Init
     
 forever:
 	sjmp forever
